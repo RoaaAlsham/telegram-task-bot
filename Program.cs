c0545926ba35410764c1c294f0f870b33bb95e32
Roaa <roaashalabalsham@gmail.com>
2026-02-05 01:03:53 +0300

    Telegram Task Bot

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using TelegramTaskBot;
+Console.WriteLine("Starting Task Bot...");
+DotNetEnv.Env.Load();
+string? token = Environment.GetEnvironmentVariable("TELEGRAM_BOT_TOKEN");
+var bot = new TaskBot(token);
+await bot.StartAsync();
+Console.ReadKey();
